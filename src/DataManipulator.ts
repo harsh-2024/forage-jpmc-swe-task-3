3685f2bcf51989db36259aedf7c223861bcbdc8e
Harsh <tripathiharsh16@gmail.com>
2023-07-09 23:31:47 +0530

    all files script fixed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  priceABC: number,
+  priceDEF: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_point: number | undefined
   timestamp: Date,
+  ratio: number
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio = priceABC/priceDEF;
+    const upper_bound = 1+0.05;
+    const lower_bound = 1-0.05;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        priceABC: priceABC,
+        priceDEF: priceDEF,
+        ratio,
+        timestamp: serverResponds[0].timestamp>serverResponds[1].timestamp?serverResponds[0].timestamp:serverResponds[1].timestamp,
+        upper_bound: upper_bound,
+        lower_bound: lower_bound,
+        trigger_point: (ratio > upper_bound || ratio < lower_bound) ? ratio : undefined,
       };
-    })
+    
   }
 }
